c6af3120e6cbc65b816b8149bdd30be2b6ed8f62
ttn2101 <ttn2101@gmail.com>
2019-02-10 12:25:12 +0700

    Add SendGrid configuration

--- GamexWeb/Identity/SendGridEmailService.cs
+++ GamexWeb/Identity/SendGridEmailService.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Configuration;
+using System.Net;
+using Microsoft.AspNet.Identity;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Mail;
+using System.Web.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace GamexWeb.Identity
 {
@@ -9,28 +14,45 @@ namespace GamexWeb.Identity
     {
         public async Task SendAsync(IdentityMessage message)
         {
-//            // Create the email object first, then add the properties.
-            
-//            
-//            // this defines email and name of the sender
-//            myMessage.From = new MailAddress("no-reply@tech.trailmax.info", "My Awesome Admin");
-//            
-//            // set where we are sending the email
-//            myMessage.AddTo(message.Destination);
-//            
-//            myMessage.Subject = message.Subject;
-//            
-//            // make sure all your messages are formatted as HTML
-//            myMessage.HtmlContent = message.Body;
-//            
-//            // Create credentials, specifying your SendGrid username and password.
-//            var credentials = new NetworkCredential("YourUsername", "YourPassword");
-//            
-//            // Create an Web transport for sending email.
-//            var transportWeb = new Web(credentials);
-//            
-//            // Send the email.
-//            await transportWeb.DeliverAsync(myMessage);
+            //            // Create the email object first, then add the properties.
+
+            //            
+            //            // this defines email and name of the sender
+            //            myMessage.From = new MailAddress("no-reply@tech.trailmax.info", "My Awesome Admin");
+            //            
+            //            // set where we are sending the email
+            //            myMessage.AddTo(message.Destination);
+            //            
+            //            myMessage.Subject = message.Subject;
+            //            
+            //            // make sure all your messages are formatted as HTML
+            //            myMessage.HtmlContent = message.Body;
+            //            
+            //            // Create credentials, specifying your SendGrid username and password.
+            //            var credentials = new NetworkCredential("YourUsername", "YourPassword");
+            //            
+            //            // Create an Web transport for sending email.
+            //            var transportWeb = new Web(credentials);
+            //            
+            //            // Send the email.
+            //            await transportWeb.DeliverAsync(myMessage);
+
+
+            var apiKey = WebConfigurationManager.AppSettings["SendGridApiKey"]; 
+            var senderEmail = "nhanttse62849@fpt.edu.vn";
+            var from = new EmailAddress(senderEmail, "Gamex Administrator");
+            var to = new EmailAddress(message.Destination);
+            var subject = message.Subject;
+            var client = new SendGridClient(apiKey);
+
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(to);
+            myMessage.From = from;
+            myMessage.Subject = subject;
+            myMessage.PlainTextContent = message.Body;
+            myMessage.HtmlContent = message.Body;
+
+            await client.SendEmailAsync(myMessage);
         }
     }
 }
